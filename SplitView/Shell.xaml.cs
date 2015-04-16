ac2a885bc7fd6afc218c32bcdee850f18ab9268c
Igor Ralic <igrali@outlook.com>
2015-04-16 22:50:26 +0200

    SplitView project added

---
+++ SplitView/Shell.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SplitView.Pages;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SplitView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Shell : Page
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+
+private void BackRadioButton_Click(object sender, RoutedEventArgs e)
+{
+    var frame = this.DataContext as Frame;
+    if (frame?.CanGoBack == true)
+    {
+        frame.GoBack();
+    }
+}
+
+private void HamburgerRadioButton_Click(object sender, RoutedEventArgs e)
+{
+    if (!this.SplitView.IsPaneOpen)
+    {
+        this.SplitView.IsPaneOpen = true;
+    }
+}
+
+private void HomeRadioButton_Click(object sender, RoutedEventArgs e)
+{
+    var frame = this.DataContext as Frame;
+    Page page = frame?.Content as Page;
+    if (page?.GetType() != typeof(HomePage))
+    {
+        frame.Navigate(typeof(HomePage));
+    }
+}
+
+private void FriendsRadioButton_Click(object sender, RoutedEventArgs e)
+{
+    var frame = this.DataContext as Frame;
+    Page page = frame?.Content as Page;
+    if (page?.GetType() != typeof(FriendsPage))
+    {
+        frame.Navigate(typeof(FriendsPage));
+    }
+}
+    }
+}
